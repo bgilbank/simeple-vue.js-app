13d3ef0873fa36c82c07cd0ba978946b6a52efe3
Brian Gilbank <bg@briangilbank.com>
2019-02-13 21:26:41 -0700

    finished vue basics

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 var app = new Vue({
-  el: '#app', // find an element with the id app
+  el: '#app', // this has to be added to the parent div, see line 9 on index.html 
   data: {
+    tasks: [
+      { id: 1, name: 'Todo 1', description: 'This is a todo', completed: false },
+      { id: 2, name: 'Todo 2', description: 'This is a todo', completed: false },
+      { id: 3, name: 'Todo THree', description: 'This is a complete todo', completed: true },
+      { id: 4, name: 'Four', description: 'This is another complete todo', completed: true }
+    ],
     message: 'Hello World'
+  },
+  computed: {
+    completedTasks: function() {
+      return this.tasks.filter( item => item.completed == true );
+    },
+
+    todoTasks: function() {
+      return this.tasks.filter( item => item.completed == false );
+    }
   }
 })
