316afdd0733464dab76f7f18c780eaa2ebcfc339
Brian Gilbank <bg@briangilbank.com>
2019-02-16 16:56:25 -0700

    added edit and delete functions to tasks

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ var app = new Vue({
   components: {
     'task': { props: ['task'],
               template: `
-              { <div class="ui segment task" 
-              v-bind:class="task.completed ? 'done' : 'todo' ">
-              {{ task.id }} {{ task.name }}: {{ task.description }} completed? {{ task.completed }}
-              </div>}
+              <div class="ui segment task" 
+                  v-bind:class="task.completed ? 'done' : 'todo' ">
+                  <div class="ui grid">
+                  <div class="left floated twelve wide column">
+                    <div class="ui checkbox">
+                      <input type="checkbox" name="task" v-on:click="app.toggleDone($event, task.id)" :checked="task.completed">
+                      <label>{{ task.name }} <span class="description">{{ task.description }}</span></label>
+                    </div>
+                  </div>
+                  </div class="right floated three wide column">
+                    <i class="icon pencil blue" alt="Edit" v-on:click="app.editTask($event, task.id)"></i>
+                    <i class="icon trash red" alt="Delete" v-on:click="app.deleteTask($event, task.id)"></i>  
+                  </div>
+                </div> 
+              </div>
               ` 
             }
   },
@@ -14,7 +25,7 @@ var app = new Vue({
     tasks: [
       { id: 1, name: 'Todo 1', description: 'This is a todo', completed: false },
       { id: 2, name: 'Todo 2', description: 'This is a todo', completed: false },
-      { id: 3, name: 'Todo THree', description: 'This is a complete todo', completed: true },
+      { id: 3, name: 'Todo Three', description: 'This is a complete todo', completed: true },
       { id: 4, name: 'Four', description: 'This is another complete todo', completed: true }
     ],
     message: 'Hello World'
@@ -26,6 +37,34 @@ var app = new Vue({
 
     todoTasks: function() {
       return this.tasks.filter( item => item.completed == false );
+    }
+  },
+  methods: {
+    toggleDone: function(event, id) {
+      event.stopImmediatePropagation(); // stops any events from occurring besides the function below.
+      let task = this.tasks.find(item => item.id == id);
+      
+      if(task) {
+        task.completed = !task.completed;
+        console.log('task toggled');
+      }
+    },
+    editTask: function(event, id){
+      let task = this.tasks.find(item => item.id == id);
+      if(task) {
+        console.log(task);
+      }
+    },
+    deleteTask(event, id){
+      event.stopImmediatePropagation();
+
+      let taskIndex = this.tasks.findIndex(item => item.id == id);
+
+      if(taskIndex > - 1){
+        this.$delete(this.tasks, taskIndex);
+      }
+
+      console.log('task deleted');
     }
   }
 })
